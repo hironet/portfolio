617dc6b23d8e3e517ea5f078517f905e08ff3e85
Hiro <hiroyuki1126@gmail.com>
2021-01-04 10:59:05 +0900

    CSSを追加。

--- toraku-ritsu/index.php
+++ toraku-ritsu/index.php
@@ -4,11 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/common.css" type="text/css">
   <title>騰落率計算ツール</title>
 </head>
 
 <body>
-  <h1>騰落率計算ツール</h1>
+<h1>騰落率計算ツール</h1>
 
 <?php
 const MIN_FORM_NUM = 1;
@@ -18,8 +19,8 @@ $form_num = DEFAULT_FORM_NUM;
 
 print <<<CTRL_FORM
 <form method="POST" action="index.php">
-  <input type="number" name="form_num" value="{$form_num}" style="width: 50px;">
-  <input type="submit" value="項目数を変更する">
+  項目数：<input class="input_form_num" type="number" name="form_num" value="{$form_num}">
+  <input class="btn_change_form_num" type="submit" value="項目数を変更する">
 </form>
 
 <form method="POST" action="index.php">
@@ -32,11 +33,11 @@ $calc_form = <<<CALC_FORM
 <form onsubmit="return false" oninput="
   hendou.value = Math.round((Number(b.value) - Number(a.value)) * 100) / 100;
   toraku.value = Math.round((Number(b.value) / Number(a.value) - 1) * 100 * 100) / 100">
-  銘柄：<input type="text" style="width: 100px;">
-  以前の株価：<input type="number" name="a" style="width: 100px;">
-  現在の株価：<input type="number" name="b" style="width: 100px;">
+  銘柄：<input type="text" class="input_brand">
+  以前の株価：<input class="input_price" type="number" name="a">
+  現在の株価：<input class="input_price" type="number" name="b">
   変動幅：<output name="hendou"></output>
-  騰落率：<output name="toraku"></output>%
+  騰落率：<output name="toraku"></output> %
   <input type="reset" value="リセット">
 </form>
 CALC_FORM;
