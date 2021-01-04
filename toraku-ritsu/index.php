30d1018484a4b014a447486cac2ed1f7568c2557
Hiro <hiroyuki1126@gmail.com>
2021-01-04 11:38:15 +0900

    軽微な修正。

--- toraku-ritsu/index.php
+++ toraku-ritsu/index.php
@@ -20,7 +20,7 @@ $form_num = DEFAULT_FORM_NUM;
 print <<<CTRL_FORM
 <form method="POST" action="index.php">
   項目数：<input class="input_form_num" type="number" name="form_num" value="{$form_num}">
-  <input class="btn_change_form_num" type="submit" value="項目数を変更する">
+  <input class="btn_change_form_num" type="submit" value="項目数を変更">
 </form>
 
 <form method="POST" action="index.php">
@@ -33,7 +33,7 @@ $calc_form = <<<CALC_FORM
 <form onsubmit="return false" oninput="
   hendou.value = Math.round((Number(b.value) - Number(a.value)) * 100) / 100;
   toraku.value = Math.round((Number(b.value) / Number(a.value) - 1) * 100 * 100) / 100">
-  銘柄：<input type="text" class="input_brand">
+  銘柄：<input class="input_brand" type="text">
   以前の株価：<input class="input_price" type="number" name="a">
   現在の株価：<input class="input_price" type="number" name="b">
   変動幅：<output name="hendou"></output>
