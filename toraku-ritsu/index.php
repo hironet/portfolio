208d463e13287f593ecf846685561e8c3f667cae
Hiro <hiroyuki1126@gmail.com>
2021-01-10 20:30:03 +0900

    軽微な修正。

--- toraku-ritsu/index.php
+++ toraku-ritsu/index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/common.css" type="text/css">
-  <title>騰落率計算ツール</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/common.css" type="text/css">
+<title>騰落率計算ツール</title>
 </head>
-
 <body>
 <h1>騰落率計算ツール</h1>
 
