d3b4f17345a1752d89d3da9dd3f7bbc43e034215
Hiro <hiroyuki1126@gmail.com>
2021-01-11 22:29:59 +0900

    新規作成。

---
+++ tetris/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/common.css" type="text/css">
+<title>テトリス</title>
+</head>
+<body>
+
+<canvas id="can"></canvas>
+<script src="js/tetris.js"></script>
+
+</body>
+</html>
