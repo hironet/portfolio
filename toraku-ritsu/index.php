ed5977a8dfdfb1582896e3290b27b8a6cf2e4d31
Hiro <hiroyuki1126@gmail.com>
2021-01-26 22:42:06 +0900

    Bootstrapを使用するように修正。

--- toraku-ritsu/index.php
+++ toraku-ritsu/index.php
@@ -3,12 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/common.css" type="text/css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <style>
-    html {
-      font-family: "メイリオ", meiryo, sans-serif;
-    }
-
     div.control {
       border-radius: 10px;
       border: 1px solid black;
