684e005b8deb4bb2ef57ec0d4320b5fe8e5d3993
Hiro <hiroyuki1126@gmail.com>
2021-03-21 22:02:09 +0900

    Googleアナリティクスのグローバルサイトタグを追加。

--- index.php
+++ index.php
@@ -382,5 +382,16 @@
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   <script src="common.js"></script>
+<?php if (strcmp($_SERVER['HTTP_HOST'], 'hironet.org') === 0) { ?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-F904Q49SRS"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-F904Q49SRS');
+  </script>
+<?php } ?>
 </body>
 </html>
