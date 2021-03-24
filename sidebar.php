9348f145ce727532885ffcc6dffedc3a26d91cf2
Hiro <hiroyuki1126@gmail.com>
2021-03-24 22:23:07 +0900

    TwitterフォローボタンとYouTubeボタンを追加。

--- sidebar.php
+++ sidebar.php
@@ -1,2 +1,8 @@
-<a class="twitter-timeline" data-lang="ja" href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw">Tweets by ITEngineerHiro</a>
+<!-- Twitterフォローボタン、タイムライン -->
+<a href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-lang="ja" data-show-count="false">Follow @ITEngineerHiro</a>
+<a class="twitter-timeline" data-lang="ja" data-chrome="noheader nofooter transparent" data-tweet-limit="5" href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw">Tweets by ITEngineerHiro</a>
 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<!-- YouTube -->
+<script src="https://apis.google.com/js/platform.js"></script>
+<div class="g-ytsubscribe" data-channelid="UCI1K0lSGrsSMckAckQ93kmA" data-layout="full" data-count="hidden"></div>
