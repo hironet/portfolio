7668ec90bd4f1ef88040c9c057a8032d0101a436
Hiro <hiroyuki1126@gmail.com>
2021-04-06 19:26:25 +0900

    Googleアナリティクスのトラッキングコードの位置を修正。

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+<?php include_once(__DIR__ . '/analyticstracking.php'); ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="このサイトはITエンジニアHiroのポートフォリオサイトです。Hiroの制作物・スキル・キャリアをご紹介しております。">
@@ -93,6 +94,5 @@
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
   <script src="common.js"></script>
-<?php include_once(__DIR__ . '/analyticstracking.php'); ?>
 </body>
 </html>
