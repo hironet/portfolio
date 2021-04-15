ab12c162298bd36daa5170f9382bc164dcd9c269
Hiro <hiroyuki1126@gmail.com>
2021-04-15 21:28:27 +0900

    新規作成。

---
+++ adsense.php
@@ -0,0 +1,3 @@
+<?php if (strcmp($_SERVER['HTTP_HOST'], 'hironet.org') === 0) { ?>
+<script data-ad-client="ca-pub-1739279744422255" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<?php } ?>
