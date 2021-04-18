ee66658ee29929b5b6f2528ef6edc0a1f7449a6e
Hiro <hiroyuki1126@gmail.com>
2021-04-18 14:38:00 +0900

    GitHubを追加。

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,17 @@
-<!-- Twitterフォローボタン、タイムライン -->
-<a href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-lang="ja" data-show-count="false">Follow @ITEngineerHiro</a>
-<a class="twitter-timeline" data-lang="ja" data-chrome="noheader nofooter transparent" data-tweet-limit="5" href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw">Tweets by ITEngineerHiro</a>
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-
+<!-- GitHub -->
+<div class="mb-3">
+  <a href="https://github.com/hironet/" target="_blank" rel="noreferrer noopener">
+    <img src="/portfolio/img/github.svg" alt="GitHub" width="100">
+  </a>
+</div>
 <!-- YouTube -->
-<script src="https://apis.google.com/js/platform.js"></script>
-<div class="g-ytsubscribe" data-channelid="UCI1K0lSGrsSMckAckQ93kmA" data-layout="full" data-count="hidden"></div>
+<div class="mb-3">
+  <script src="https://apis.google.com/js/platform.js"></script>
+  <div class="g-ytsubscribe" data-channelid="UCI1K0lSGrsSMckAckQ93kmA" data-layout="full" data-count="hidden"></div>
+</div>
+<!-- Twitterフォローボタン、タイムライン -->
+<div class="mb-3">
+  <a href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-lang="ja" data-show-count="false">Follow @ITEngineerHiro</a>
+  <a class="twitter-timeline" data-lang="ja" data-chrome="noheader nofooter transparent" data-tweet-limit="5" href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw">Tweets by ITEngineerHiro</a>
+  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
