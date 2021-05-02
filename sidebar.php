47be41e6d5945abdfadc13b614808bf5a46a5a84
Hiro <hiroyuki1126@gmail.com>
2021-05-02 22:32:41 +0900

    Twitterの高さを設定。

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,6 @@
 <!-- Twitterフォローボタン、タイムライン -->
 <div class="mb-3">
   <a href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-lang="ja" data-show-count="false">Follow @ITEngineerHiro</a>
-  <a class="twitter-timeline" data-lang="ja" data-chrome="noheader nofooter transparent" data-tweet-limit="5" href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw">Tweets by ITEngineerHiro</a>
+  <a class="twitter-timeline" data-height="300" data-lang="ja" data-chrome="noheader nofooter transparent" data-tweet-limit="5" href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw">Tweets by ITEngineerHiro</a>
   <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
