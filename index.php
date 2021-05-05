23c6b0ba43ea21a2504452db59ec1b9160a4a438
Hiro <hiroyuki1126@gmail.com>
2021-05-05 11:22:54 +0900

    Googleアドセンスのコードを削除。

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ $contents_desc = 'このサイトはITエンジニアHiroのポートフォリ�
 <html lang="ja">
 <head>
 <?php include_once(__DIR__ . '/analyticstracking.php'); ?>
-<?php include_once(__DIR__ . '/adsense.php'); ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="<?= $contents_desc ?>">
