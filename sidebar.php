df6ccb94fd2beacc13d9a18fa6c083037d999226
Hiro <hiroyuki1126@gmail.com>
2021-05-06 21:30:55 +0900

    YouTubeへのリンクを削除。

--- sidebar.php
+++ sidebar.php
@@ -4,8 +4,3 @@
     <img src="/portfolio/img/github.svg" alt="GitHub" width="100">
   </a>
 </div>
-<!-- YouTube -->
-<div class="mb-3">
-  <script src="https://apis.google.com/js/platform.js"></script>
-  <div class="g-ytsubscribe" data-channelid="UCI1K0lSGrsSMckAckQ93kmA" data-layout="full" data-count="hidden"></div>
-</div>
