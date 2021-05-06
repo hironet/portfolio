3b00461ab777e7bad8be5b162b26c1d7758ff0c7
Hiro <hiroyuki1126@gmail.com>
2021-05-06 16:29:55 +0900

    Twitterを削除。

--- sidebar.php
+++ sidebar.php
@@ -9,9 +9,3 @@
   <script src="https://apis.google.com/js/platform.js"></script>
   <div class="g-ytsubscribe" data-channelid="UCI1K0lSGrsSMckAckQ93kmA" data-layout="full" data-count="hidden"></div>
 </div>
-<!-- Twitterフォローボタン、タイムライン -->
-<div class="mb-3">
-  <a href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-lang="ja" data-show-count="false">Follow @ITEngineerHiro</a>
-  <a class="twitter-timeline" data-height="300" data-lang="ja" data-chrome="noheader nofooter transparent" data-tweet-limit="5" href="https://twitter.com/ITEngineerHiro?ref_src=twsrc%5Etfw">Tweets by ITEngineerHiro</a>
-  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-</div>
