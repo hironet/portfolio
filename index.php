0ee565a717b2231c0f1fff4f64d7ad666187d30b
Hiro <hiroyuki1126@gmail.com>
2021-05-09 10:55:09 +0900

    common.jsのURLを変更。

--- index.php
+++ index.php
@@ -101,6 +101,6 @@ $contents_desc = 'このサイトはITエンジニアHiroのポートフォリ�
     </div>
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  <script src="common.js"></script>
+  <script src="https://engineer-hiro.com/common.js"></script>
 </body>
 </html>
