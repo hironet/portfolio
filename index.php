dbf2a2db6dcae284898fcde7db05b979fcfa8413
Hiro <hiroyuki1126@gmail.com>
2021-05-11 20:51:58 +0900

    bootstrap5.0.0にバージョンアップ。

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $contents_desc = 'このサイトはITエンジニアHiroのポートフォリ�
   <meta name="twitter:description" content="<?= $contents_desc ?>">
   <meta name="twitter:image" content="https://engineer-hiro.com/portfolio/img/ogp.png">
   <!-- /OGP -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
   <link rel="icon" href="/portfolio/img/favicon.svg" type="image/svg+xml">
   <link rel="apple-touch-icon" href="/portfolio/img/apple-touch-icon.png">
   <title><?= $contents_title ?></title>
@@ -100,7 +100,7 @@ $contents_desc = 'このサイトはITエンジニアHiroのポートフォリ�
       <small>&copy; <?= date('Y') ?> ITエンジニア Hiro</small>
     </div>
   </footer>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
   <script src="https://engineer-hiro.com/common.js"></script>
 </body>
 </html>
