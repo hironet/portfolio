f70057d56bbe1dd826b3f9ab724eaf64d9d8c0d2
Hiro <hiroyuki1126@gmail.com>
2021-05-16 21:50:50 +0900

    新規作成。

---
+++ common.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', function() {
+  // スムーズにスクロールする処理
+  (() => {
+    let Ease = {
+      easeInOut: (t) => t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2 ) + 1
+    };
+    const duration = 500;
+    const scrollTriggers = document.querySelectorAll('a[href^="#"]');
+    scrollTriggers.forEach((scrollTrigger) => {
+      scrollTrigger.addEventListener('click', (e) => {
+        let href = scrollTrigger.getAttribute('href');
+        let targetElement = document.getElementById(href.replace('#', ''));
+        if (targetElement) {
+          e.preventDefault();
+          e.stopPropagation();
+          const rect = targetElement.getBoundingClientRect().top;
+          const header = document.getElementById('nav');
+          const headerHeight = header.offsetHeight;
+          const target = rect + window.pageYOffset - headerHeight;
+          const currentPostion = document.documentElement.scrollTop || document.body.scrollTop;  // クロスブラウザ対策
+          const startTime = performance.now();
+          let loop = (nowTime) => {
+            let time = nowTime - startTime;
+            let normalizedTime = time / duration;
+            if (normalizedTime < 1) {
+              window.scrollTo(0, currentPostion + ((target - currentPostion) * Ease.easeInOut(normalizedTime)));
+              requestAnimationFrame(loop);
+            } else {
+              window.scrollTo(0, target);
+            }
+          }
+          requestAnimationFrame(loop);
+        }
+      });
+    });
+  })();
+});
