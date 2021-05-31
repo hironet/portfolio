bc9e93814d838a4c697c29ef512a1aa646a73dbe
Hiro <hiroyuki1126@gmail.com>
2021-05-31 22:54:45 +0900

    軽微な修正。

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ $contents_desc = 'このサイトはITエンジニアHiroのポートフォリ�
   <hr class="featurette-divider">
   <footer>
     <div class="container text-center my-3">
-      <small>&copy; <?= date('Y') ?> ITエンジニア Hiro</small>
+      <small>&copy; <?= date('Y') ?> ITエンジニアHiro</small>
     </div>
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
