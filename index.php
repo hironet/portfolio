8cab8371650ddf6698516f9ea8ed8b4f733da195
Hiro <hiroyuki1126@gmail.com>
2021-06-28 08:11:23 +0900

    CSSを外部ファイル化。

--- index.php
+++ index.php
@@ -20,67 +20,7 @@ $contents_desc = 'このサイトはITエンジニアHiroのポートフォリ�
   <link rel="apple-touch-icon" href="/img/apple-touch-icon.png">
   <title><?= $contents_title ?></title>
   <style>
-    body {
-      padding-top: 2rem;
-    }
-
-    div.box {
-      border-radius: 8px;
-      border: solid 3px;
-      margin-bottom: 1rem;
-      padding: 0.5rem;
-      position: relative;
-    }
-
-    h2.skill-class {
-      font-size: 1.2rem;
-      text-decoration: underline;
-    }
-
-    #nav {
-      opacity: 0.9;
-    }
-
-    /* ページトップに戻るボタン */
-    button#page-top {
-      /* buttonタグのリセットCSS */
-      appearance: none;
-      border: none;
-      cursor: pointer;
-      outline: none;
-      padding: 0;
-
-      /* ボタンの装飾 */
-      align-items: center;
-      background-color: #212529;
-      border-radius: 50%;
-      bottom: 15px;
-      display: flex;
-      height: 50px;
-      justify-content: center;
-      opacity: 0.7;
-      position: fixed;
-      right: 15px;
-      transition: all 0.3s ease;
-      width: 50px;
-      z-index: 100;
-    }
-
-    /* ページトップに戻るボタン：矢印 */
-    button#page-top::before {
-      border-right: solid 3px #fff;
-      border-top: solid 3px #fff;
-      content: "";
-      height: 12px;
-      margin-bottom: -6px;
-      transform: rotate(-45deg);
-      width: 12px;
-    }
-
-    /* ページトップに戻るボタン：ホバー時 */
-    button#page-top:hover {
-      transform: scale(1.1);
-    }
+<?php include_once(__DIR__ . '/style.php'); ?>
   </style>
 </head>
 <body id="top">
