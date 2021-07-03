263bd3219e26089c50c92ccf7c6a9e1557231be3
Hiro <hiroyuki1126@gmail.com>
2021-07-03 12:03:06 +0900

    軽微な修正。

--- index.php
+++ index.php
@@ -8,14 +8,14 @@ $contents_desc = 'このサイトはITエンジニアHiroのポートフォリ�
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="<?= $contents_desc ?>">
-  <!-- OGP -->
+  <!-- Twitter -->
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:url" content="https://engineer-hiro.com/">
   <meta name="twitter:title" content="<?= $contents_title ?>">
   <meta name="twitter:description" content="<?= $contents_desc ?>">
   <meta name="twitter:image" content="https://engineer-hiro.com/img/ogp.png">
-  <!-- /OGP -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+  <!-- /Twitter -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
   <link rel="icon" href="/img/favicon.svg" type="image/svg+xml">
   <link rel="apple-touch-icon" href="/img/apple-touch-icon.png">
   <title><?= $contents_title ?></title>
