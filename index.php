c804c855510abb4f3c72a0a39d2b64d358c13573
Hiro <hiroyuki1126@gmail.com>
2021-07-04 14:58:00 +0900

    favicon.pngを追加。

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ $contents_desc = 'このサイトはITエンジニアHiroのポートフォリ�
   <!-- /Twitter -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
   <link rel="icon" type="image/svg+xml" href="/img/favicon.svg">
+  <link rel="icon alternate" type="image/png" href="{{ secure_asset('/img/favicon.png') }}">
   <link rel="apple-touch-icon" href="/img/apple-touch-icon.png">
   <title><?= $contents_title ?></title>
   <style>
